6c34462aea5084719bccc903172745d313d89b8f
GALGALLOR <69774313+GALGALLOR@users.noreply.github.com>
2021-09-21 09:54:05 +0300

    made it Post

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from instabot import Bot
 from flask import Flask,request,render_template,redirect
 import time
 import re
+import random
+import pyautogui
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys #for pressing keys on the keyboard in chrome
-names='jabbling2020'
-passy='galojabbling04'
+names='its.galo_2'
+passy='KCD831J'
 
 app=Flask(__name__)
 PATH ="C:/Users/LENOVO/Downloads/chromedriver.exe"
@@ -32,11 +35,24 @@ def post_to_gram(names,passy):
     print('Im in!')
     time.sleep(10)
     try:
+        time.sleep(5)
+        not_now2=driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
+        print('found the Not now button')
+        not_now2.send_keys(Keys.ENTER)
+        time.sleep(5)
+    except:
+        print('couldnt find the not now 2 button')
+    try:
         post=driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[3]/div/button')
         post.send_keys(Keys.ENTER)
-        print("success")
+        print("found the Post Button")
     except:
         print('Could not find the post button')
+    
+    select_from_computer=driver.find_element_by_xpath('/html/body/div[9]/div/div/div/div[2]/div[2]/div[2]/div/button')
+    print('found the Choose an image button')
+    select_from_computer.send_keys(Keys.ENTER)
+    print('Choose an image')
 
 
 def obtain_images():
@@ -46,21 +62,25 @@ def obtain_images():
     response = requests.get(url2)
     soup=BeautifulSoup(response.text,'html.parser')
     global h3
-    h3 = soup.find_all('h3')
+    h3 = soup.find_all('p')
     global images
     images=soup.find_all('img')
-    """global list_of_captions
-    list_of_captions=[]"""
+    global list_of_captions
+    list_of_captions=[]
     global list_of_images
     list_of_images=[]
-    """
+    
     global words
     global captions
     for captions in list_of_captions:
-    pass
+        pass
     for words in h3:
-        words=words['title']
-        list_of_captions.append(words)"""
+        words=str(words)
+        words=words.replace('<p>','')
+        words=words.replace('</p>','')
+        print(words)
+        #words=words['title']
+        list_of_captions.append(words)
     global image
     for image in images:
         image=image['src']
@@ -70,6 +90,15 @@ def obtain_images():
     global dps
     for dps in list_of_images:
         pass
+
+def download_image2(images):
+    url =images
+    url=str(url)
+    r = requests.get(url, allow_redirects=True)
+    global file
+    file=random.randint(1,100000000000)
+    file=str(file)+'.png'
+    open(file, 'wb').write(r.content)
 
 def download_image(image):
     fifi='//*[@id="app"]/div/div[1]/div[2]/input'
@@ -87,24 +116,74 @@ def download_image(image):
     time.sleep(60)
     print('download complete')
 
-obtain_images()
-ldnf=0
-for image in list_of_images:
-    try:
-        ldnf=ldnf+1
-        print('image ',ldnf)
-        print(image)
-        download_image(image)
-        
-    except:
-        print('testing the alternative')
-        pass
+
+
+def execute_download_image():
+    ldnf=0
+    for image in list_of_images:
+        try:
+            ldnf=ldnf+1
+            print('image ',ldnf)
+            print(image)
+            if 'gif' in image:
+                print('cannot work on a gif image')
+            elif 'us' in image:
+                print('error found...passing')
+            elif 'facebook' in image:
+                print('cannot download logos... passing')
+            else:
+                print('Proper image found')
+                download_image2(image)
+                print('successfully downloaded')
+                time.sleep(5)
+                post_to_gram_2(file)
+                
+            
+        except:
+            print('testing the alternative')
+            pass
+
+
+def scan_area():
+    for n in range(1,1000):
+        time.sleep(1)
+        print(pyautogui.position())
+def post_to_gram_2(file):
+    print('posting to gram')
+    post_to_gram(names,passy)
+
+    #maximize=(982,24)
+    #pyautogui.click(maximize)
+
+
+ 
+ 
+bot = Bot()
+ 
+bot.login(username = "user_name",
+          password = "user_password")
+ 
+# Recommended to put the photo
+# you want to upload in the same
+# directory where this Python code
+# is located else you will have
+# to provide full path for the photo
+bot.upload_photo("",
+                 caption ="Techn"
+
+
+#post_to_gram_2()
+
+
+#obtain_images()
+#execute_download_image()
+
 #post_to_gram(names,passy)
 #@app.route('/')
 #def home():
 #    return render_template('text.html',images=list_of_images,captions=list_of_captions)
 
 
-if __name__=='__main__':
-    app.debug=True
-    app.run()
+#if __name__=='__main__':
+#    app.debug=True
+#    app.run()
