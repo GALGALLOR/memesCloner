4c508eab954a826ffb2129c9a0133261050c0e9e
GALGALLOR <69774313+GALGALLOR@users.noreply.github.com>
2021-09-23 01:57:46 +0300

    idk what ive done here

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-from instabot import Bot
+import instaloader
 from flask import Flask,request,render_template,redirect
 import time
 import os
@@ -15,47 +15,57 @@ from selenium.webdriver.common.keys import Keys #for pressing keys on the keyboa
 
 
 
-bot = Bot()
- 
-bot.login(username = "jabbling2020",
-          password = "galojabbling04")
+
+#from instabot import Bot
+#bot = Bot()
+#bot.login(username = "jabbling2020",
+#          password = "galojabbling04")
 
 
 def obtain_images():
     url='https://www.football365.com/'
     url2='https://www.dailymail.co.uk/sport/football/index.html'
+    url3='https://sportslens.com/tags/competitions/english-premier-league/'
+    url4='https://www.sportsmole.co.uk/'
+    url5='http://inbedwithmaradona.com/'
+    url6='https://www.diariogol.com/'
+    url7='https://www.instagram.com/433'
     HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
-    response = requests.get(url2)
+    response = requests.get(url7)
     soup=BeautifulSoup(response.text,'html.parser')
-    global h3
-    h3 = soup.find_all('p')
-    global images
+    
+    #h2 = soup.find_all('h2')
     images=soup.find_all('img')
-    global list_of_captions
-    list_of_captions=[]
+    #global list_of_captions
+    #list_of_captions=[]
     global list_of_images
     list_of_images=[]
     
-    global words
-    global captions
-    for captions in list_of_captions:
-        pass
-    for words in h3:
-        words=str(words)
-        words=words.replace('<p>','')
-        words=words.replace('</p>','')
-        print(words)
-        #words=words['title']
-        list_of_captions.append(words)
-    global image
+
+    
     for image in images:
         image=image['src']
+        print(image)
         list_of_images.append(image)
+
+    
     
 
-    global dps
-    for dps in list_of_images:
-        pass
+    '''for words in images:
+        words=words['alt']
+        print(words)
+        title=str(words)['title']
+        #words=str(words)
+        #words=words.split('>')
+        #words=words[2]
+        #words=str(words)
+        #words=words.split('<')
+        #words=words[0]
+        list_of_captions.append(words)'''
+    
+    
+
+    
 
 
 def download_image2(images):
@@ -71,13 +81,12 @@ def download_image2(images):
 
 
 
-def execute_download_image():
+def dailymail_download_image(list_of_images,list_of_captions):
+    captionx=0
     ldnf=0
-    xlf=0
     for image in list_of_images:
         try:
            ldnf=ldnf+1
-           
            print('image ',ldnf)
            print(image)
            if 'gif' in image:
@@ -94,7 +103,7 @@ def execute_download_image():
                print('successfully downloaded')
                time.sleep(5)
                try:
-                   bot.upload_photo(file,caption =list_of_captions[xlf])
+                   bot.upload_photo(file,caption)
                   
                    print('caption number:',xlf)
                    xlf=xlf+1
@@ -117,6 +126,97 @@ def execute_download_image():
 
 
 
+def get_instagram_caption(username):
+   # Create an instaloader object with parameters
+    L = instaloader.Instaloader(download_pictures = False, download_videos = False, download_comments= False, compress_json = False)
+   
+    # Log in with the instaloader object
+    L.login("jabbling2020" , "galojabbling04")
+    # Search the instagram profile
+    profile = instaloader.Profile.from_username(L.context, username)
+    print('success')
+    # Scrape the posts
+    posts = profile.get_posts()
+    duration=0
+    for post in posts: 
+        duration=duration+1
+        L.download_post(post, target = profile.username)
+        time=str(post.date)
+        time=time.replace(' ','_')
+        time=time.replace(':','-')
+        time='433/'+time+'_UTC.txt'
+        f = open(time, "r",encoding='utf8')
+        caption=f.read()
+        global list_of_captions
+        list_of_captions=[]
+        list_of_captions.append(caption)
+        
+        if duration==5:
+            break
 
-obtain_images()
-execute_download_image()
+def instagram_download_image(list_of_images,list_of_captions):
+    captionx=0
+    ldnf=0
+    for image in list_of_images:
+        try:
+           ldnf=ldnf+1
+           print('image ',ldnf)
+           print(image)
+           if 'gif' in image:
+               print('cannot work on a gif image')
+           elif 'us' in image:
+               print('error found...passing')
+           elif 'i.dailymail.co.uk/i/pix' in image:
+               print('Possible logo found')
+           elif 'facebook' in image:
+               print('cannot download logos... passing')
+           else:
+               print('Proper image found')
+               download_image2(image)
+               print('successfully downloaded')
+               time.sleep(5)
+               try:
+                   bot.upload_photo(file,caption)
+                  
+                   print('caption number:',xlf)
+                   xlf=xlf+1
+                   print('successfully posted')
+                   time.sleep(10)
+                   os.remove(file)
+                   print(file)
+               except:
+                   xlf=xlf+1
+                   print(file)
+                   os.remove(file)
+                   print('Could not post the image')
+                   time.sleep(2)
+                   
+                   print('deleteing the Image')
+        except:
+            print('testing the alternative')
+            pass
+        
+def obtain_images_instagram():
+    url='https://knowyourmeme.com/'
+    HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
+    response = requests.get(url)
+    soup=BeautifulSoup(response.text,'html.parser')
+    images=soup.find_all('img')
+    global list_of_images
+    list_of_images=[] 
+
+    for image in images:
+        image=image['src']
+        print(image)
+        list_of_images.append(image)
+    
+
+
+
+obtain_images_instagram()
+#print(list_of_images)
+#get_instagram_caption('433')
+#print(list_of_captions)
+
+
+
